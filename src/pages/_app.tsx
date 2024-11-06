ad41cf25e0648b28634acaa12aecf17b7a3a0d1e
Burak Erdem <falsemech@gmail.com>
2024-11-06 20:00:16 +0300

    chore: Pausible Provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,18 @@ import "../../styles/globals.css";
 import type { AppProps } from "next/app";
 
 import { Toaster } from "react-hot-toast";
+import PausibleProvider from "next-plausible";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <PausibleProvider
+      domain="burakerdem.com"
+      selfHosted
+      customDomain="https://analytics.burakerdem.com"
+    >
       <Component {...pageProps} />
       <Toaster />
-    </>
+    </PausibleProvider>
   );
 }
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,13 +9,6 @@ export default function Document() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Rajdhani:200,300,400|Roboto:300,400|Petit+Formal+Script:400&display=swap"
         />
-        <script
-          src="https://analytics.burakerdem.com/js/plausible.js"
-          async
-          defer
-          data-domain="burakerdem.com"
-          data-nscript="afterInteractive"
-        ></script>
       </Head>
       <body>
         <Main />
